c875b102da978af3304713cf3dacc75b93aa7dcf
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-08-08 18:36:29 +0200

    Update tests

--- examples/test.sh
+++ examples/test.sh
@@ -32,24 +32,6 @@ $ANGSD/angsd -glf $SIM_DATA/testF.glf.gz -fai $SIM_DATA/testAF.ANC.fas.fai -anc 
 
 
 
-##### Calculate covariance matrix
-gunzip -f testF.indF.geno.gz testF.indF.saf.gz
-$NGSPOPGEN/ngsCovar -probfile testF.indF.geno -outfile testF.indF.covar -nind 20 -nsites $N_SITES -call 0 -sfsfile testF.indF.saf -norm 0
-
-
-
-##### Calculate population genetics statistics
-$NGSPOPGEN/ngsStat -npop 1 -postfiles testF.indF.saf -nsites $N_SITES -iswin 1 -nind 20 -outfile testF.indF.stat -block_size $N_SITES
-
-
-
-##### SFS
-N_IND=20
-# Calculating SFS
-hexdump -v -s 8 -e "$((2*N_IND+1))/4 \"%.10g\t\"\"\n\"" testF.indF.saf | perl -na -e '$sum=0; $sum+=exp($_) for @F; next if($sum==0); for $i (0..$#F){$frq[$i]+=exp($F[$i])/$sum}; END{$tsum+=$_ for @frq; $_/=$tsum for @frq; print join("\t",@frq)."\n"}' > testF.indF.saf_sum
-
-
-
 ##### Check MD5
 rm -f *.arg
 TMP=`mktemp --suffix .ngsF`
